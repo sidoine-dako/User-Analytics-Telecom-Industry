385532cdedf4e2d86129e3e87fc02e9765eda9ed
sidoine-dako <audedako@gmail.com>
2022-05-10 00:24:23 +0100

    Update Extract_data.py

--- scripts/Extract_data.py
+++ scripts/Extract_data.py
@@ -7,32 +7,39 @@ class extract_data:
     
     def extract_SocialMedia(self,identifier:str):
         SocialMediaCol = [col for col in self.df.columns if 'Social Media' in col]
-        SocialMediaData = pd.Series(self.df[SocialMediaCol].groupby(identifier).sum().sum(1),name='Social Media')
+        SocialMediaCol.append(identifier)
+        print(SocialMediaCol)
+        SocialMediaData = pd.Series(self.df[SocialMediaCol].groupby(identifier).sum(1),name='Social Media')
         return SocialMediaData
 
     def extract_Google(self,identifier:str):
         GoogleCol = [col for col in self.df.columns if 'Google' in col]
-        GoogleData = pd.Series(self.df[GoogleCol].groupby(identifier).sum().sum(1),name="Google")
+        GoogleCol.append(identifier)
+        GoogleData = pd.Series(self.df[GoogleCol].groupby(identifier).sum(1),name="Google")
         return GoogleData
 
     def extract_Email(self,identifier:str):
         EmailCol = [col for col in self.df.columns if 'Email' in col]
-        EmailData = pd.Series(self.df[EmailCol].groupby(identifier).sum().sum(1),name="Email")
+        EmailCol.append(identifier)
+        EmailData = pd.Series(self.df[EmailCol].groupby(identifier).sum(1),name="Email")
         return EmailData
 
     def extract_Youtube(self,identifier:str):
         YoutubeCol = [col for col in self.df.columns if 'Youtube' in col]
-        YoutubeData = pd.Series(self.df[YoutubeCol].groupby(identifier).sum().sum(1),name="Youtube")
+        YoutubeCol.append(identifier)
+        YoutubeData = pd.Series(self.df[YoutubeCol].groupby(identifier).sum(1),name="Youtube")
         return YoutubeData
 
     def extract_Gaming(self,identifier:str):
         GamingCol = [col for col in self.df.columns if 'Gaming' in col]
-        GamingData = pd.Series(self.df[GamingCol].groupby(identifier).sum().sum(1),name="Gaming")
+        GamingCol.append(identifier)
+        GamingData = pd.Series(self.df[GamingCol].groupby(identifier).sum(1),name="Gaming")
         return GamingData
 
     def extract_Other(self,identifier:str):
-        OtherCol = [col for col in self.df.columns if 'Youtube' in col]
-        OtherData = pd.Series(self.df[OtherCol].groupby(identifier).sum().sum(1),name="Other")
+        OtherCol = [col for col in self.df.columns if 'Other' in col]
+        OtherCol.append(identifier)
+        OtherData = pd.Series(self.df[OtherCol].groupby(identifier).sum(1),name="Other")
         return OtherData
 
     def merge_data(self,identifier:str):
