8902337d4a570da1051a79db5f7f1d706a1a6690
sidoine-dako <audedako@gmail.com>
2022-05-09 23:19:51 +0100

    Create Extract_data.py
    Class to extract the data for each application

---
+++ scripts/Extract_data.py
@@ -0,0 +1,12 @@
+import pandas as pd
+class extract_data:
+    def __init__(self,df:pd.DataFrame):
+        self.df =df
+    
+    def extract_SocialMedia(self):
+        SocialMediaCol = [col for col in self.df.columns if 'Social Media' in col]
+        return self.df[SocialMediaCol].sum(1)
+
+    def extract_Google(self):
+        GoogleCol = [col for col in self.df.columns if 'Google' in col]
+        return self.df[GoogleCol].sum(1)
