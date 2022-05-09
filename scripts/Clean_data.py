b6de6707b9aac3b7905de8a4c7aa58cda7e2a642
sidoine-dako <audedako@gmail.com>
2022-05-09 17:02:04 +0100

    Update module for data pre-processing

--- scripts/Clean_data.py
+++ scripts/Clean_data.py
@@ -1,4 +1,12 @@
 import pandas as pd
+import numpy as np
 class clean_data:
     def __init__(self,df:pd.DataFrame):
         self.df = df
+
+    def missing_values(self,verbose=True):
+        # Count missing values per column
+        missingCountCol = self.df.isna().sum()
+
+        # Count the total number of missing values
+        missingCountTot = missingCountCol.sum()
