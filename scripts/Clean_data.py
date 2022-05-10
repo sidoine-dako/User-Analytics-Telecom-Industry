6a81a9b278ba46606eb0ac286c214aed46a8fab1
sidoine-dako <audedako@gmail.com>
2022-05-10 21:43:46 +0100

    Update Clean_data.py
    Fix some bugs

--- scripts/Clean_data.py
+++ scripts/Clean_data.py
@@ -1,3 +1,4 @@
+from xmlrpc.client import Boolean
 import pandas as pd
 import numpy as np
 class clean_data:
@@ -28,26 +29,36 @@ class clean_data:
 
         return missingCountCol, missingCountTot, missingPerc
 
-    def fillMissing(self):
+    def fillMissing(self,func=None):
         """This function fill the NA with either the mean or the median.
         It uses the mean if the distribution of the column is symmetric and the median otherwise."""
         numCols = self.df.select_dtypes([np.number]).columns
-        skewList = self.df[numCols].skew()
-        for col in skewList.index:
-            if abs(skewList[col])>1:
+        if func == "mean":
+            for col in numCols:
+                valToFill = self.df[col].mean()
+                self.df[col] = self.df[col].fillna(valToFill)
+        elif func =="median":
+            for col in numCols:
                 valToFill = self.df[col].median()
                 self.df[col] = self.df[col].fillna(valToFill)
-            else:
-                valToFill = self.df[col].median()
-                self.df[col] = self.df[col].fillna(valToFill)
+        else:
+            skewList = self.df[numCols].skew()
+            for col in skewList.index:
+                if abs(skewList[col])>1:
+                    valToFill = self.df[col].median()
+                    self.df[col] = self.df[col].fillna(valToFill)
+                else:
+                    valToFill = self.df[col].mean()
+                    self.df[col] = self.df[col].fillna(valToFill)
 
     def remove_duplicate(self,col:list)->pd.DataFrame:
         """This function remove the duplicates based on the variables listed in 'list' """
         dfWithoutDuplicated = self.df.drop_duplicates(col)
         return dfWithoutDuplicated
 
-    def detect_outliers(self,col:list):
+    def detect_outliers(self):
         """This function returns the outliers in a data set"""
+        col = self.df.select_dtypes([np.number]).columns
         Q1 = self.df[col].quantile(.25)
         Q3 = self.df[col].quantile(.75)
         IQR = Q3-Q1
@@ -60,7 +71,7 @@ class clean_data:
             outlierList = np.where((self.df.loc[:,colname] > Q3[colname] + 1.5 * IQR[colname]) | (self.df.loc[:,colname] < Q1[colname] - 1.5 * IQR[colname]))
             outlierList = list(outlierList[0])
             outliersCol[colname] = outlierList
-        return outliersCount, outlierList
+        return outliersCount, outlierList, outliersCol
 
 if __name__ == "__main__":
     df = pd.read_csv("..\data\Week1_challenge_data_source(CSV).csv")
