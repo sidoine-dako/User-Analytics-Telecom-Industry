90e750da5ee90bcf1e4415e706f7215b404a115e
sidoine-dako <audedako@gmail.com>
2022-05-11 15:11:47 +0100

    Update Extract_data.py

--- scripts/Extract_data.py
+++ scripts/Extract_data.py
@@ -56,6 +56,14 @@ class extract_data:
         TotalData = pd.Series(self.df[TotalCol].groupby(identifier).sum().sum(1),name="Total")
         return TotalData
 
+    def extract_TotalUL(self,identifier:str,totalULCol='Total UL (Bytes)'):
+        TotalUL = self.df[[identifier,totalULCol]].groupby(identifier).sum()
+        return TotalUL
+
+    def extract_TotalDL(self,identifier:str,totalDLCol='Total DL (Bytes)'):
+        TotalDL = self.df[[identifier,totalDLCol]].groupby(identifier).sum()
+        return TotalDL
+    
     def merge_data(self,identifier:str):
         NSession = self.extract_Session(identifier)
         sumDuration = self.sum_duration(identifier)
@@ -65,9 +73,11 @@ class extract_data:
         YoutubeData = self.extract_Youtube(identifier)
         GamingData = self.extract_Gaming(identifier)
         OtherData = self.extract_Other(identifier)
+        TotalUL = self.extract_TotalUL(identifier)
+        TotalDL = self.extract_TotalDL(identifier)
         TotalData = self.extract_Total(identifier)
         try:
-            Data = [NSession,sumDuration,SocialMediaData,GoogleData,EmailData,YoutubeData,GamingData,OtherData,TotalData]
+            Data = [NSession,sumDuration,SocialMediaData,GoogleData,EmailData,YoutubeData,GamingData,OtherData,TotalUL,TotalDL,TotalData]
             df_final = ft.reduce(lambda left,right: pd.merge(left,right,left_index=True,right_index=True,
         validate="one_to_one"),Data)
         except KeyError:
