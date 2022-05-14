bca2f33e86f33d0cc01692383c3174a829b91484
sidoine-dako <audedako@gmail.com>
2022-05-14 13:31:49 +0100

    Update mainapp.py

--- dashboard/mainapp.py
+++ dashboard/mainapp.py
@@ -36,18 +36,19 @@ df = pd.read_csv("./data/UniqueUser.csv")
 st.title('Tellco Data Analysis')
 st.subheader("This is a basic dashboard to present the user overview")
 # Table for the topn handset type
+st.subheader('Top handset type')
 topn=st.number_input(label='Top n handset type',min_value=1,max_value=50,value=10,help="Choose a number to print the top handset type",key=1)
-
 dfTopHandsetType = topN(df,'Handset Type',topn,undefined=False)
 st.write(dfTopHandsetType)
 
 # Table for the topn manufacturer
+st.subheader('Top handset manufacturer')
 topm=st.number_input(label='Top m handset manufacturer',min_value=1,max_value=50,value=10,key=2)
-
 dfTopHandsetMan = topN(df,'Handset Manufacturer',topm,undefined=False)
 st.write(dfTopHandsetMan)
 
 # Table of the top n type of handset for the top m manufacturer
+st.subheader('Top handset type by manufacturer')
 topn2=st.number_input(label='Top n handset type',min_value=1,max_value=10,value=3,help="Choose a number to print the top handset type",key=3)
 topm2=st.number_input(label='Top m handset manufacturer',min_value=1,max_value=10,value=5,key=4)
 dfTopHandTMan = topTypeManufact(df,topm2,topn2)
